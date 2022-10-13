7f664eabdff29d6c5eae4433d580e33a51903424
halafi <filiphalas74@gmail.com>
2022-10-13 12:01:09 +0700

    chore: clean config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,18 +1,21 @@
-fish_add_path $GOBIN
-fish_add_path $GOROOT/bin
-fish_add_path /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+# DISABLED UNTIL NEEDED
+#fish_add_path $GOBIN
+#fish_add_path $GOROOT/bin
+#fish_add_path /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+#fish_add_path /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
 # pipx
-fish_add_path ~/.local/bin
-fish_add_path $HOME/.poetry/bin
-fish_add_path $HOME/tools/lua-language-server/bin
-fish_add_path $HOME/.cargo/bin
-# output of $(yarn global bin)
-fish_add_path /Users/halafi/.asdf/installs/nodejs/16.14.2/.npm/bin
-fish_add_path /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
+#fish_add_path ~/.local/bin
+#fish_add_path $HOME/.poetry/bin
+#fish_add_path $HOME/Library/Application\ Support/pypoetry/venv/bin
+#fish_add_path $HOME/tools/lua-language-server/bin
+#fish_add_path $HOME/.cargo/bin
 
+# output of $(yarn global bin)
+fish_add_path ~/.asdf/installs/nodejs/16.17.1/.npm/bin
+
+# need to load brew binaries
 eval "$(/opt/homebrew/bin/brew shellenv)"
-source (brew --prefix asdf)/asdf.fish
-fish_add_path $HOME/Library/Application\ Support/pypoetry/venv/bin
+source (brew --prefix asdf)/libexec/asdf.fish
 
 # disable greeting
 set fish_greeting
