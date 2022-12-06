0829f56cdd02baf66dd149faf55315f3e4fe33f1
halafi <filiphalas74@gmail.com>
2022-12-06 13:55:07 +0700

    feat: update finicky

--- home/finicky.js
+++ home/finicky.js
@@ -1,26 +1,37 @@
+const WORK_RELATED_HOSTS = [
+  "zenhub.com",
+  "app.zenhub.com",
+  "github.com",
+  "zoom.us",
+  "cloud.google.com",
+  "chrome.google.com",
+  "drive.google.com",
+  "figma.com",
+];
+
 module.exports = {
   defaultBrowser: "Firefox",
   options: {
-    hideIcon: true, 
+    hideIcon: true,
     checkForUpdate: true,
-    logRequests: false
+    logRequests: false,
   },
   rewrite: [
     {
       // Redirect all urls to use https
       match: ({ url }) => url.protocol === "http",
-      url: { protocol: "https" }
-    }
+      url: { protocol: "https" },
+    },
   ],
   handlers: [
     {
-      match: ({ url }) => url.host === "zenhub.com" || url.host === "app.zenhub.com" || url.host === "github.com",
-      browser: "Google Chrome"
+      match: ({ url }) => WORK_RELATED_HOSTS.includes(url.host),
+      browser: "Google Chrome",
     },
-    {
-      // Open any url that includes the string "workplace" in Firefox
-      match: /drive.google/,
-      browser: "Google Chrome"
-    }
-  ]
+    // {
+    //   // Open any url that includes the string "workplace" in Firefox
+    //   match: /drive.google/,
+    //   browser: "Google Chrome",
+    // },
+  ],
 };
